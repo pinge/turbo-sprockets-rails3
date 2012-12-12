e514384d101ceb1b32e55e9cea078aef4f8ccab8
Nuno Pinge <nuno.pinge@livestream.com>
2012-12-12 14:57:57 -0500

    added parallel asset precompilation support

--- lib/turbo-sprockets/sprockets_overrides/static_compiler.rb
+++ lib/turbo-sprockets/sprockets_overrides/static_compiler.rb
@@ -23,62 +23,91 @@ if defined?(Sprockets::StaticCompiler)
         @source_digests = {}
       end
 
+      def processor_count
+        begin
+          @@cpu_cores ||= case RbConfig::CONFIG['host_os']
+                            when /darwin9/
+                              `hwprefs cpu_count`.to_i
+                            when /darwin/
+                              ((`which hwprefs` != '') ? `hwprefs thread_count` : `sysctl -n hw.ncpu`).to_i
+                            when /linux/
+                              `cat /proc/cpuinfo | grep processor | wc -l`.to_i
+                            when /freebsd/
+                              `sysctl -n hw.ncpu`.to_i
+                            when /mswin|mingw/
+                              require 'win32ole'
+                              wmi = WIN32OLE.connect("winmgmts://")
+                              cpu = wmi.ExecQuery("select NumberOfCores from Win32_Processor") # TODO count hyper-threaded in this
+                              cpu.to_enum.first.NumberOfCores
+                            else
+                              1
+                          end
+        rescue Exception => e
+          @@cpu_cores = 1
+        end
+      end
+
       def compile
-        start_time = Time.now.to_f
-
-        env.each_logical_path do |logical_path|
-          if File.basename(logical_path)[/[^\.]+/, 0] == 'index'
-            logical_path.sub!(/\/index\./, '.')
-          end
-          next unless compile_path?(logical_path)
-
-          # Fetch asset without any processing or compression,
-          # to calculate a digest of the concatenated source files
-          if asset = env.find_asset(logical_path, :process => false)
-            @source_digests[logical_path] = asset.digest
-
-            # Recompile if digest has changed or compiled digest file is missing
-            current_digest_file = @current_digests[logical_path]
-
-            if @source_digests[logical_path] != @current_source_digests[logical_path] ||
-               !(current_digest_file && File.exists?("#{@target}/#{current_digest_file}"))
-
-              if asset = env.find_asset(logical_path)
-                @digests[logical_path] = write_asset(asset)
-                # Update current_digests with new hash, for future assets to reference
-                @current_digests[logical_path] = asset.digest_path
-              end
-
+        logical_paths = env.each_logical_path.reject do |logical_path|
+          logical_path.sub!(/\/index\./, '.') if File.basename(logical_path)[/[^\.]+/, 0] == 'index'
+          !compile_path?(logical_path)
+        end
+        if processor_count > 1
+          Rails.application.config.sass.cache = false
+          Rails.application.config.sass.read_cache = false
+        end
+        workers = []
+        (processor_count * 2).times do
+          workers << worker(logical_paths)
+        end
+        reads = workers.map { |worker| worker[:read] }
+        writes = workers.map { |worker| worker[:write] }
+        index = finished = 0
+        loop do
+          break if finished >= logical_paths.size
+          # monitor workers reads (precompiled assets) and writes (logical paths indexes)
+          ready = IO.select(reads, writes)
+          ready[0].each do |readable|
+            # get precompiled asset from worker/child process
+            job = Marshal.load(readable)
+            @source_digests[job[:logical_path]] = job[:asset_digest]
+            if job[:processed]
+              @digests[job[:logical_path]] = job[:asset]
+              # Update current_digests with new hash, for future assets to reference
+              @current_digests[job[:logical_path]] = job[:asset_digest_path]
             else
-              # Set asset file from manifest.yml
-              digest_file = @current_digests[logical_path]
-              @digests[logical_path] = digest_file
-
-              env.logger.debug "Not compiling #{logical_path}, sources digest has not changed " <<
-                               "(#{@source_digests[logical_path][0...7]})"
+              digest_file = @current_digests[job[:logical_path]]
+              @digests[job[:logical_path]] = digest_file
             end
+            finished += 1
+          end
+          ready[1].each do |write|
+            break if index >= logical_paths.size
+            Marshal.dump([index, @source_digests], write)
+            index += 1
           end
         end
-
+        workers.each do |worker|
+          worker[:read].close
+          worker[:write].close
+        end
+        workers.each do |worker|
+          Process.wait worker[:pid]
+        end
         # Encode all filenames & digests as UTF-8 for Ruby 1.9,
         # otherwise YAML dumps other string encodings as !binary
         if RUBY_VERSION.to_f >= 1.9
           @source_digests = encode_hash_as_utf8 @source_digests
           @digests        = encode_hash_as_utf8 @digests
         end
-
         if @manifest
           write_manifest(@digests)
           write_sources_manifest(@source_digests)
         end
-
         # Store digests in Rails config. (Important if non-digest is run after primary)
         config = ::Rails.application.config
         config.assets.digests        = @digests
         config.assets.source_digests = @source_digests
-
-        elapsed_time = ((Time.now.to_f - start_time) * 1000).to_i
-        env.logger.debug "Processed #{'non-' unless @digest}digest assets in #{elapsed_time}ms"
       end
 
       def write_sources_manifest(source_digests)
@@ -88,11 +117,59 @@ if defined?(Sprockets::StaticCompiler)
         end
       end
 
+      def worker(logical_paths)
+        child_read, parent_write = IO.pipe # pipe to get messages from parent process
+        parent_read, child_write = IO.pipe # pipe to send messages to parent process
+        pid = fork do
+          begin
+            parent_write.close
+            parent_read.close
+            while !child_read.eof?
+              logical_path_index, source_digests = Marshal.load(child_read) # get asset logical path to process from parent
+              logical_path = logical_paths[logical_path_index] # get asset logical path to process from parent
+              if asset = env.find_asset(logical_path, :process => false)
+                source_digests[logical_path] = asset.digest
+                # Recompile if digest has changed or compiled digest file is missing
+                current_digest_file = @current_digests[logical_path]
+                #if false
+                if source_digests[logical_path] != @current_source_digests[logical_path] ||
+                    !(current_digest_file && File.exists?("#{@target}/#{current_digest_file}"))
+
+                  if asset = env.find_asset(logical_path)
+                    Marshal.dump({
+                                     :processed => true,
+                                     :logical_path => logical_path,
+                                     :asset => write_asset(asset),
+                                     :asset_digest => source_digests[logical_path],
+                                     :asset_digest_path => asset.digest_path
+                                 }, child_write)
+                  end
+                else
+                  Marshal.dump({
+                                   :processed => false,
+                                   :logical_path => logical_path,
+                                   :asset_digest => source_digests[logical_path]
+                               }, child_write)
+                end
+              end
+
+            end
+          ensure # close pipes file descriptors even if something goes wrong
+            child_read.close
+            child_write.close
+          end
+        end
+        child_read.close
+        child_write.close
+        { :read => parent_read, :write => parent_write, :pid => pid }
+      end
+
       private
 
       def encode_hash_as_utf8(hash)
         Hash[*hash.map {|k,v| [k.encode("UTF-8"), v.encode("UTF-8")] }.flatten]
       end
+
     end
   end
 end
